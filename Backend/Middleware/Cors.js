cd1e1d1f772d21ec81ff2359c654ab63b0016180
SADHASIVAM-VM <sadha4747@gmail.com>
2024-06-21 12:40:43 +0530

    ig

--- Backend/Middleware/Cors.js
+++ Backend/Middleware/Cors.js
@@ -1,7 +1,7 @@
 const allowedOrigins = process.env.URL;
 const corsOptions = {
     origin: function (origin, callback) {
-        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+        if (allowedOrigins.includes(origin)) {
             callback(null, true);
         } else {
             callback(new Error('Not allowed by CORS'));
