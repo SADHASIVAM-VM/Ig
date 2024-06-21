a91f56177b13fae32353830835a79f63354d3f6a
SADHASIVAM-VM <sadha4747@gmail.com>
2024-06-21 12:54:51 +0530

    ig

--- Backend/Middleware/Cors.js
+++ Backend/Middleware/Cors.js
@@ -1,11 +1,12 @@
-const allowedOrigins = process.env.URL;
+const allowedOrigins = "https://ig-iota-umber.vercel.app/";
 const corsOptions = {
-    origin: function (origin, callback) {
-        if (allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
+  origin: (origin, callback) => {
+    if (allowedOrigins.includes(origin)|| !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
     }
+  },
 };
+
 module.exports = corsOptions;
